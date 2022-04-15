4c5f6c4eb27fac4cc3772ddf8dbd17409ec2ad4f
spock-the-wizard <jeson@postech.ac.kr>
2022-04-16 03:53:26 +0900

    added args to frame extractor

--- utils/vid_util.py
+++ utils/vid_util.py
@@ -1,7 +1,14 @@
 import os
 import cv2
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--video',default='./data/faces/face1.mp4',type=str,help='path of video to extract frames')
+
+args = parser.parse_args()
 
 FRAME_DIR = './data/frames/'
+PRINT_STATUS_FREQUENCY=100
 def extract_frames(vid_pth,frame_dir=None):
     if frame_dir is None:
         frame_dir = FRAME_DIR
@@ -11,6 +18,8 @@ def extract_frames(vid_pth,frame_dir=None):
     if not os.path.exists(frame_dir):
         os.makedirs(frame_dir)
     
+    
+    print('Start extraction from %s...'%vid_pth)
     cap = cv2.VideoCapture(vid_pth)
     cnt = 0
     while cap.isOpened():
@@ -19,12 +28,12 @@ def extract_frames(vid_pth,frame_dir=None):
             break
         cv2.imwrite(os.path.join(frame_dir,'%d.png'%cnt),image)
         
-        # import pdb;pdb.set_trace()
         cnt += 1
+        if cnt%PRINT_STATUS_FREQUENCY == 0:
+            print('Processed %d frames'%cnt)
+
         
     cap.release()
 
-if __name__=="__main__":
-    # import pdb;pdb.set_trace()
-    pth = './data/faces/test2.mp4'
-    extract_frames(pth)
+if __name__=="__main__":    
+    extract_frames(args.video)
